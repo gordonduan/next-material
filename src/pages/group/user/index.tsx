3eb8aa8200b7bc1f22dabfb23691ff5c49107b22
gordonduan <gordon.duan@orbitremit.com>
2021-06-03 15:42:24 +1200

    Adding repositories and user api

--- src/pages/group/user/index.tsx
+++ src/pages/group/user/index.tsx
@@ -1,31 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import UserTable from '../../../components/User/UserTable';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+const User = () => {
+  const [data, setData] = useState([]);
+  // const [pagination, setPagination] = useState({
+  //   current: 1,
+  //   pageSize: 10
+  // });
+  // const [query, setQuery] = useState({params: pagination});
 
-const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
-];
+  // const [isLoading, setIsLoading] = useState(false);
 
-const index = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/user');
+      console.log(result);
+      setData(result.data.data.list);
+    };
+    fetchData();
+  }, []);
+
+  // const handleTableChange = (pagination, filters, sorter) => {
+  //   setQuery({
+  //     params: {
+  //       current: pagination.current,
+  //       pageSize: pagination.pageSize
+  //     }
+  //   });
+  // };
+
   return (
     <UserTable
-      rows={rows}
+      rows={data}
     />
-  )
+  );
 }
 
-export default index;
+export default User;
